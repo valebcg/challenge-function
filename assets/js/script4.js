e277c67a4dd125e299caed461bf1ba4c257ac28c
valebcg <valeria.cortes.g@gmail.com>
2022-07-17 20:57:34 -0400

    fix

--- assets/js/script4.js
+++ assets/js/script4.js
@@ -2,28 +2,28 @@ let element1= document.querySelector ("#div1")
 let element2= document.querySelector ("#div2")
 let element3= document.querySelector ("#div3")
 let element4= document.querySelector ("#div4")
-let color= 'aqua'
+
 element1.setAttribute('style', 'width: 200px; height: 200px; background-color: red;')
 element2.setAttribute('style', 'width: 200px; height: 200px; background-color: green;')
 element3.setAttribute('style', 'width: 200px; height: 200px; background-color: yellow;')
 element4.setAttribute('style', 'width: 200px; height: 200px; background-color: blue;')
+let color= 'aqua'
 
 document.addEventListener('keydown', function (event) { 
-    if (event.key.toLowerCase() === 'a') {
+    if (event.key === 'a') {
      color= 'orange'
-    } else if (event.key.toLowerCase() === 's') { 
+    } else if (event.key === 's') { 
       color= 'blue'
-    } else if (event.key.toLowerCase() === 'd'){
+    } else if (event.key === 'd'){
         color= 'yellow'
-    } else if (event.key.toLowerCase() === 'f'){
+    } else if (event.key  === 'f'){
         color = 'pink'
     }
- 
-})
+ })
 
-    let setColor = (element) => {
+   let setColor = (element) => {
   element.addEventListener('click', () => {
-    document.querySelector('#' + element.id).style.backgroundColor = color
+    element.style.backgroundColor = color
   })
 }
 setColor(element1),setColor(element2),setColor(element3),setColor(element4)
